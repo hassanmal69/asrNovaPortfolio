2ef7e0af4af497995256f78adb10c5da70d6bb04
abdulmoiz180 <moiza8684@gmail.com>
2025-03-26 02:39:19 +0500

    added work component

--- src/pages/home/components/bookaCall/index.tsx
+++ src/pages/home/components/bookaCall/index.tsx
@@ -4,7 +4,7 @@ const BookaCall = () => {
     return (
         <section className="relative w-full flex justify-center items-center">
             <div className="relative w-full">
-                <BackgroundBeams />
+                <BackgroundBeams children={undefined} />
             </div>
             <div className="TextContent absolute w-full flex">
                 <h1 className="text-6xl">Book a Call Now!</h1>
