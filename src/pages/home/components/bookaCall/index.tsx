a74b9ce62e7ed8f8591e282bb3cabe1fb4b8ae0e
hassanmal69 <158709739+hassanmal69@users.noreply.github.com>
2025-03-27 01:23:47 -0700

    workMade

--- src/pages/home/components/bookaCall/index.tsx
+++ src/pages/home/components/bookaCall/index.tsx
@@ -9,10 +9,10 @@ const BookaCall = () => {
                 <BackgroundBeams children={undefined} />
             </div>
             <div className="TextContent absolute w-full flex gap-3.5 flex-col">
-                <h1 className="text-4xl md:text-4xl leading-normal lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+                <h1 className="text-4xl md:text-4xl leading-normal right lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
                     Smart Sites, Bigger Profits <br /> with <Cover>Asr Nova</Cover>
                 </h1>
-                <p className="w-1/2 text-lg text-gray-300 !m-auto text-center">
+                <p className="w-1/2 text-lg lato text-gray-300 !m-auto text-center">
                     We create high-converting websites that help your business grow and make more money. Book a free consultation call and let’s chat about your goals!</p>
                 <FormBookacall />
             </div>
---
+++ src/pages/home/components/work/animatedmoon/index.tsx
@@ -0,0 +1,8 @@
+import './moon.css'
+const MoonComponent = () => {
+  return (
+    <div className="animated-moon"></div>
+  )
+}
+
+export default MoonComponent
